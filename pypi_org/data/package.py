83a8d68b107d135bb6239e0db882df83620efbdf
Jacob Biehler <biehlerj@gmail.com>
2019-11-13 21:45:11 -0800

    Updating imports and adding create_session method in db_session

--- pypi_org/data/package.py
+++ pypi_org/data/package.py
@@ -4,26 +4,26 @@ from typing import List
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 
-from data.modelbase import SqlAlchemyBase
-from data.releases import Release
+from pypi_org.data.modelbase import SqlAlchemyBase
+from pypi_org.data.releases import Release
 
 
 class Package(SqlAlchemyBase):
     __tablename__ = 'packages'
-    id = sa.Column(sa.String, primary_key=True)
-    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
-    summary = sa.Column(sa.String, nullable=False)
-    description = sa.Column(sa.String, nullable=True)
 
-    home_page = sa.Column(sa.String)
-    docs_url = sa.Column(sa.String)
-    package_url = sa.Column(sa.String)
+    id: str = sa.Column(sa.String, primary_key=True)
+    created_date: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    summary: str = sa.Column(sa.String, nullable=False)
+    description: str = sa.Column(sa.String, nullable=True)
 
-    author_name = sa.Column(sa.String)
-    author_email = sa.Column(sa.String, index=True)
+    home_page: str = sa.Column(sa.String)
+    docs_url: str = sa.Column(sa.String)
+    package_url: str = sa.Column(sa.String)
 
-    license = sa.Column(sa.String, index=True)
+    author_name: str = sa.Column(sa.String)
+    author_email: str = sa.Column(sa.String, index=True)
 
+    license: str = sa.Column(sa.String, index=True)
     # releases relationship
     releases: List[Release] = orm.relation('Release', order_by=[
         Release.major_ver.desc(),
